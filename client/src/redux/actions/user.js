5b293da6512ca55069491bee3c6a69b5add6d166
Khattak <akkhattak65@gmail.com>
2020-05-21 10:18:22 +0500

    :bug: Fixed Minor bug

--- client/src/redux/actions/user.js
+++ client/src/redux/actions/user.js
@@ -7,34 +7,34 @@ export const LOGOUT_USER = "LOGOUT_USER";
 export const LOAD_USER_DATA = "LOAD_USER_DATA";
 export const LOAD_USER_DATA_FAILED = "LOAD_USER_DATA_FAILED";
 
-export const loginUser = (email, password) => dispatch => {
+export const loginUser = (email, password) => (dispatch) => {
   dispatch(layoutActions.loadingStart());
   const config = {
     headers: {
-      "Content-type": "application/json"
-    }
+      "Content-type": "application/json",
+    },
   };
 
   const body = JSON.stringify({
     email,
-    password
+    password,
   });
 
   axios
     .post("auth/login", body, config)
-    .then(res => {
+    .then((res) => {
       dispatch({
         type: LOGIN_USER,
         payload: {
           user: res.data.user,
-          isAuth: true
-        }
+          isAuth: true,
+        },
       });
       localStorage.setItem("token", res.data.token);
       dispatch(layoutActions.loadingEnd());
       dispatch(layoutActions.snackbarShow("success", res.data.msg[0]));
     })
-    .catch(err => {
+    .catch((err) => {
       dispatch(layoutActions.loadingEnd());
       if (err.response) {
         return dispatch(
@@ -50,38 +50,39 @@ export const loginUser = (email, password) => dispatch => {
     });
 };
 
-export const loadUserData = (
-  userId = undefined,
-  cb = undefined
-) => dispatch => {
+export const loadUserData = (userId = undefined, cb = undefined) => (
+  dispatch
+) => {
   axios
     .get(`/user/get${userId ? "/" + userId : ""}`)
-    .then(res => {
+    .then((res) => {
       dispatch({
         type: LOAD_USER_DATA,
-        payload: res.data
+        payload: res.data,
       });
       dispatch({
-        type: LOADING_END
+        type: LOADING_END,
       });
     })
-    .catch(err => {
+    .catch((err) => {
       dispatch({
-        type: LOAD_USER_DATA_FAILED
+        type: LOAD_USER_DATA_FAILED,
       });
       dispatch({
-        type: LOADING_END
+        type: LOADING_END,
       });
-      cb();
+      if (!!cb) {
+        cb();
+      }
     });
 };
 
-export const logout = () => dispatch => {
+export const logout = () => (dispatch) => {
   localStorage.removeItem("token");
   dispatch({
-    type: LOGOUT_USER
+    type: LOGOUT_USER,
   });
   dispatch({
-    type: LOADING_END
+    type: LOADING_END,
   });
 };
